ad7f4b4040d0ad16a9e8e4d558584f80a1560309
Alfonso Tech <alfonsoteched@nym.hush.com>
2024-03-19 22:26:41 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// console.log("hello from the js file");
+
+// //Primitive Data Type
+// //string
+// ("hello");
+// ("34");
+// "as owisn wer &&&"`hello, someone`; // later
+
+// //numbers
+// 34;
+// 35.7;
+// 883 - 23;
+// 0;
+// NaN; //typeof number
+
+// //boolean
+// true;
+// false;
+
+//undefined
+//like a question that has been asked but no answer or value is received.
+
+//null
+//like a question that has been asnwer in the negative
+
+//Variable
+//let (value changes)
+let counter = 1;
+let dessert = null;
+
+let lucrative;
+let lucrative2 = undefined;
+
+//const (constant)  // Data Storage
+const friend = "Sabina"; //string
+const length = 34; //number
+const isCold = false; //boolean
+
+//typeof
+console.log("counter:", counter);
+
+console.log("dessert:", dessert);
+console.log("typeof dessert:", typeof dessert);
+
+console.log("lucrative, lucrative2", lucrative, lucrative2);
+
+console.log("typeof friend", typeof friend);
+
+console.log(typeof length);
+console.log(typeof isCold);
+
+//length
+console.log("friend", friend);
+console.log("typeof friend", typeof friend);
+console.log("friend.length", friend.length); //
